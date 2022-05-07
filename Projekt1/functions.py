a577049daa420facd9bc38084f6ad3bcb22d9800
ItamiOdio <32541839+ItamiOdio@users.noreply.github.com>
2022-05-07 03:12:17 +0200

    Info, gain, split info

--- Projekt1/functions.py
+++ Projekt1/functions.py
@@ -1,16 +1,75 @@
+from calc_values import count_occur, occur_prob
 import math
+import copy
 
 # Wyliczenie entropii
 def calc_entropy(dict_of_chance):
-    entr = 0
+    entropy = 0
     for key in dict_of_chance:
         p = dict_of_chance[key]
         if p != 0:
-            entr += p * math.log2(p)
+            entropy += p * math.log2(p)
             #print(entropy)
-    entr = entr * -1
-    return entr
+    entropy = entropy * -1
+    return entropy
 
-def calc_info():
+def calc_info_main(data, prob_count, columns):
+    info = []
+    data_t = copy.deepcopy(data)
+    data_t = list(map(list, zip(*data_t)))
+    for i in range(columns-1):
+        attr_indexes = (find_attributes(data_t,prob_count, i))
+        print(attr_indexes)
+        info_temp = calc_info(attr_indexes, data, prob_count, i)
+        info.append(info_temp)
+
+    return info
     pass
+
+def calc_gain(attr_info, entropy):
+    gain = entropy - attr_info
+    return gain
+
+def find_attributes(data, key_list, index):
+    index_list = []
+    #print(key_list)
+    for key in key_list[index]:
+        value = [i for i, x in enumerate(data[index]) if x == key]
+        index_list.append(value)
+    return index_list
+
+def calc_info(attr_indexes, data, prob_count, ind):
+    info = 0
+    print('Poczatek')
+    temp_entr = []
+
+    for i in (attr_indexes):
+        temp = []
+        for j in i:
+            temp.append(data[j])
+            temp_occ = count_occur(len(temp), len(temp[0]), temp)
+            temp_prob = occur_prob(len(temp), len(temp[0]), temp_occ)
+
+        temp_entr.append(calc_entropy(temp_prob[len(temp[0])-1]))
+    print(temp_entr)
+
+    k = 0
+    for key in prob_count[ind]:
+        prob = prob_count[ind][key]
+        ent = temp_entr[k]
+        info += prob * ent
+        k += 1
+
+    print()
+    print(info)
+    print('Koniec')
+    return info
+
+
+
+
+
+
+
+
 
--- Projekt1/main.py
+++ Projekt1/main.py
@@ -1,20 +1,28 @@
 from calc_values import count_occur, occur_prob
-from functions import calc_entropy, calc_info
+from functions import calc_entropy, calc_info_main, calc_gain
+import copy
 
 f = open("gielda.txt", "r")
 data = [[x for x in line.rstrip("\n").split(',')] for line in f]
-data_t = list(map(list, zip(*data)))
-
 rows = len(data)
 columns = len(data[0])
 print(rows)
 
 occurances = count_occur(rows, columns, data)
 print(occurances)
+probabilities = occur_prob(rows, columns, copy.deepcopy(occurances))
 
-probabilities = occur_prob(rows, columns, occurances)
 print(probabilities)
 
 # Oblicz entropię dla atrybutu decyzyjnego
 ent = calc_entropy(probabilities[columns-1])
 print(ent)
+
+info = calc_info_main(data, probabilities, columns)
+print(info)
+
+gain = [(calc_gain(info[i], ent)) for i in range(len(info))]
+print(gain)
+
+split_info = [(calc_entropy(probabilities[i])) for i in range(columns-1)]
+print(split_info)
